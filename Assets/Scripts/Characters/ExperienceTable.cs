37c20190eed662074b7b7a54bc680f499b00179f
vkaytsanov <vkaytsanov@yahoo.com>
2021-04-21 12:49:01 +0300

    character stats with needed utillity methods

---
+++ Assets/Scripts/Characters/ExperienceTable.cs
@@ -0,0 +1,27 @@
+﻿namespace Characters {
+
+	public static class ExperienceTable {
+		/** Diablo 2 experiences per level, up to the 20th level
+		 *  @link https://diablo.fandom.com/wiki/Character_Level
+		 */
+		private static readonly int[] Requirement = {
+			0, 500, 1_500, 3_750, 7_875, 14_175, 22_680, 32_886, 44_396, 57_715, 72_144, 90_180,
+			112_725, 140_906, 176_132, 220_165, 275_207, 344_008, 430_010, 537_513
+		};
+
+		public static int GetNextLevelExperienceRequirement(int level) {
+			return Requirement[level % Requirement.Length];
+		}
+
+		/** e.g. if we are at level 1 and we have 510 experience, we should level up */
+		public static bool ShouldLevelUp(int level, int experience) {
+			return Requirement[level % Requirement.Length] < experience;
+		}
+
+		/** Should be called for the UI */
+		public static int ExperienceLeft(int level, int experience) {
+			return Requirement[level % Requirement.Length] - experience;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Characters/Stats.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Characters {
+
+	public enum StatsType {
+		// Core Stats
+		Strength = 0,
+		Stamina = 1,
+		Charisma = 2,
+		
+		// Leveling Stats
+		Experience = 3,
+		Level = 4
+	}
+	
+	public class Stats {
+		private int _availablePoints = 0;
+		private const int PointsPerLevel = 5;
+		/** Points in the stat */
+		public readonly int[] Points = new int[Enum.GetNames(typeof(StatsType)).Length];
+
+		public int Strength {
+			get => Points[0];
+			set => Points[0] = value;
+		}
+
+		public int Stamina {
+			get => Points[1];
+			set => Points[1] = value;
+		}
+
+		public int Charisma {
+			get => Points[2];
+			set => Points[2] = value;
+		}
+
+		public int Experience {
+			get => Points[3];
+			set => Points[3] = value;
+		}
+
+		public int Level {
+			get => Points[4];
+			set => Points[4] = value;
+		}
+
+		// TODO Play with the coefficients scaling 
+		public int Damage => Strength * 10;
+		public int Health => Stamina * 10;
+		public float AdditionalGold => Charisma * 0.2f;
+
+		public int GetPointsInStat(StatsType statsType) {
+			return Points[(int) statsType];
+		}
+
+		private void OnLevelUp() {
+			// leveling up
+			Level++;
+			// adding points to spend
+			_availablePoints += PointsPerLevel;
+			// resetting the experience
+			Experience = 0;
+		}
+
+		public void OnMobKilled(int experience) {
+			Experience += experience;
+
+			if (ExperienceTable.ShouldLevelUp(Level, Experience)) {
+				OnLevelUp();
+			}
+		}
+
+		/** Should be called for the UI */
+		public int GetAvailablePoints() {
+			return _availablePoints;
+		}
+
+		/** Should be called from the UI */
+		public void OnPointSpend(StatsType statsType) {
+			Points[(int) statsType]++;
+			_availablePoints--;
+		}
+	}
+
+}
